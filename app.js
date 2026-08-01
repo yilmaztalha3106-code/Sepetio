ba87d39a46ff2f852a598f939dffd85611d08ccc
yilmaztalha3106-code <yilmaztalha3106@gmail.com>
2026-08-01 16:52:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 let cart = [];
 
 function addToCart(productName, price) {
+
     cart.push({
         name: productName,
         price: price
     });
 
+    document.getElementById("cartCount").innerText = cart.length;
+
     alert(productName + " sepete eklendi!");
-    
-    console.log(cart);
+}
+
+
+function showCart() {
+
+    if(cart.length === 0){
+        alert("Sepetiniz boş.");
+        return;
+    }
+
+    let message = "Sepetiniz:\n\n";
+
+    cart.forEach(item => {
+        message += item.name + " - " + item.price + " TL\n";
+    });
+
+    alert(message);
 }
