3048507ba3eb053631bff3a4320a3e786e0cff00
yilmaztalha3106-code <yilmaztalha3106@gmail.com>
2026-08-03 10:23:55 +0300

    Update ürünler.js

--- ürünler.js
+++ ürünler.js
@@ -1,1 +1,72 @@
+/* ==========================================================
+   SEPETIO V2
+   Ürün Veritabanı (Geçici)
+========================================================== */
 
+const urunler = [
+
+{
+    id:1,
+    ad:"Kablosuz Kulaklık Pro",
+    fiyat:1299,
+    kategori:"Elektronik",
+    resim:"https://picsum.photos/400/400?random=11"
+},
+
+{
+    id:2,
+    ad:"Akıllı Saat X",
+    fiyat:2799,
+    kategori:"Elektronik",
+    resim:"https://picsum.photos/400/400?random=12"
+},
+
+{
+    id:3,
+    ad:"Oyuncu Klavyesi RGB",
+    fiyat:1099,
+    kategori:"Teknoloji",
+    resim:"https://picsum.photos/400/400?random=13"
+},
+
+{
+    id:4,
+    ad:"Bluetooth Hoparlör",
+    fiyat:899,
+    kategori:"Ses",
+    resim:"https://picsum.photos/400/400?random=14"
+},
+
+{
+    id:5,
+    ad:"Oyuncu Mouse",
+    fiyat:699,
+    kategori:"Teknoloji",
+    resim:"https://picsum.photos/400/400?random=15"
+},
+
+{
+    id:6,
+    ad:"4K Monitör",
+    fiyat:7499,
+    kategori:"Bilgisayar",
+    resim:"https://picsum.photos/400/400?random=16"
+},
+
+{
+    id:7,
+    ad:"Mekanik Klavye",
+    fiyat:1699,
+    kategori:"Bilgisayar",
+    resim:"https://picsum.photos/400/400?random=17"
+},
+
+{
+    id:8,
+    ad:"Powerbank 20000 mAh",
+    fiyat:1199,
+    kategori:"Elektronik",
+    resim:"https://picsum.photos/400/400?random=18"
+}
+
+];
